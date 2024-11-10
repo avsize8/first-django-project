47a823485a31999f548ecb501931cf207198f643
avsize8 <158048434+avsize8@users.noreply.github.com>
2024-11-10 10:03:53 +0500

    Add files via upload

--- main/views.py
+++ main/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import TaskForm
 from .models import Task
 # Create your views here.
 
 def index(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.order_by('-id')
     return render(request, 'main/index.html',{'title': 'Главная страница сайта','tasks':tasks})
 
 
 def about(request):
     return render(request, 'main/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'invalid form'
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
